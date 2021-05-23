9f15547ac2167340e69ee2120b5bf677f7192fe1
denev <65989857+deneb0723@users.noreply.github.com>
2021-05-23 22:24:13 +0900

    movie data api 작성

--- src/api.js
+++ src/api.js
@@ -9,8 +9,27 @@ const api = axios.create({
 });
 
 export const movieApi = {
-  topRated: () => api.get("/movie/top_rated"),
   nowPlaying: () => api.get(`/movie/now_playing`),
+  topRated: () => api.get("/movie/top_rated"),
+  popular: () => api.get("/movie/popular"),
+  upcoming: () => api.get("/movie/upcoming"),
+  movieDetail: (id) =>
+    api.get(`/movie/${id}`, { params: { append_to_response: "video" } }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: { query: encodeURIComponent(term) },
+    }),
 };
 
-export const tvApi = {};
+export const tvApi = {
+  airingToday: () => api.get("/tv/airing_today"),
+  onTheAir: () => api.get("/tv/on_the_air"),
+  popular: () => api.get("/tv/popular"),
+  topRated: () => api.get("/tv/top_rated"),
+  tvDetail: (id) =>
+    api.get(`/tv/${id}`, { params: { append_to_response: "video" } }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: { query: encodeURIComponent(term) },
+    }),
+};
